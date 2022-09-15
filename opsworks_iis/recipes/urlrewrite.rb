c046651b51e96b3fed96cee4206a3a335436fb48
Gennady Trubach <10942688+gtrubach@users.noreply.github.com>
2022-09-15 09:59:47 +0200

    Update IIS Rewrite download URL (#2)

--- opsworks_iis/recipes/urlrewrite.rb
+++ opsworks_iis/recipes/urlrewrite.rb
@@ -1,5 +1,5 @@
-iis_urlrewrite_path = ::File.join(Chef::Config["file_cache_path"], "rewrite_2.0_rtw_x64.msi")
-iis_urlrewrite_url = "http://go.microsoft.com/?linkid=9722532"
+iis_urlrewrite_path = ::File.join(Chef::Config["file_cache_path"], "rewrite_amd64_en-US.msi")
+iis_urlrewrite_url = "https://download.microsoft.com/download/1/2/8/128E2E22-C1B9-44A4-BE2A-5859ED1D4592/rewrite_amd64_en-US.msi"
 
 remote_file iis_urlrewrite_path do
   source iis_urlrewrite_url
